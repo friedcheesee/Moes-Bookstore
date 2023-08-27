ab761115b2051dfa676c00ff150f5dbc0cdac18f
friedcheesee <piyush20152003@gmail.com>
2023-08-27 14:05:40 +0530

    all works, mult packages

---
+++ part2/funct.go
@@ -0,0 +1,7 @@
+package part2
+
+import "fmt"
+
+func Add(a int, b int) {
+	fmt.Println(a + b)
+}
---
+++ try.go
@@ -0,0 +1,9 @@
+package main
+
+import "example/part2"
+
+func main() {
+	a := 4
+	b := 5
+	part2.Add(a, b)
+}
